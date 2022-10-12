0b1d160ffa2598a30c15e68849219fd1e38fec7c
Simon Wu <109928703+siwuagain@users.noreply.github.com>
2022-10-11 22:07:18 -0400

    Add files via upload
    functioning page with most css styling completed, still missing @media styling for screen size changes

--- sign-up-form.js
+++ sign-up-form.js
@@ -1,1 +1,22 @@
+const user_phone_number = document.getElementById('user_phone_number');
 
+//automatically format phone number as it is inputed
+user_phone_number.addEventListener('input', (event) => {
+  let phone_number = event.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  event.target.value = !phone_number[2] ? phone_number[1] : '(' + phone_number[1] + ') ' + phone_number[2] + (phone_number[3] ? '-' + phone_number[3] : '');
+});
+
+const user_password = document.getElementById('user_password');
+
+const confirm_user_password = document.getElementById('confirm_user_password');
+
+const password_match_alert = document.getElementById('password_match_alert');
+
+//check if passwords match at each input
+confirm_user_password.addEventListener('input', () => {
+  if (user_password.value != confirm_user_password.value) {
+    password_match_alert.textContent = "*Your passwords do not match";
+  } else {
+    password_match_alert.textContent = '';
+  }
+})
